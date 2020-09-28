1b76d1385b219838b14e6dffdb53b70508164736
Henry Tran <henryhtran07@gmail.com>
2020-09-28 12:16:02 -0700

    added case

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use('/', indexRouter);
 app.use('/signup', signupRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
